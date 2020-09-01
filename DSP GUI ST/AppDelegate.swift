1f5e14b32bbd768bbd113a77ecfe0f1a68b8ae2b
Ilya Yelagov <yelagov@gmail.com>
2020-09-01 23:18:29 +0300

    Initial Commit

---
+++ DSP GUI ST/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DSP GUI ST
+//
+//  Created by ilyayelagov on 9/1/20.
+//  Copyright © 2020 ilyayelagov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DSP GUI STTests/DSP_GUI_STTests.swift
@@ -0,0 +1,34 @@
+//
+//  DSP_GUI_STTests.swift
+//  DSP GUI STTests
+//
+//  Created by ilyayelagov on 9/1/20.
+//  Copyright © 2020 ilyayelagov. All rights reserved.
+//
+
+import XCTest
+@testable import DSP_GUI_ST
+
+class DSP_GUI_STTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
